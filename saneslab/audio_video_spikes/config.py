37842f392b26325d21a760a3b4937e61c884ae4c
Jeremy Magland <jmagland@flatironinstitute.org>
2023-01-23 08:38:24 -0500

    add .avi file

--- saneslab/audio_video_spikes/config.py
+++ saneslab/audio_video_spikes/config.py
@@ -1,5 +1,5 @@
 # /mnt/home/rpeterson/ceph/ephys_audio_video_better
 video_timestamps_npy_uri = 'sha1://60daf012cf10f00e10224e7516b79b2a71603694?label=ifr_20Hz.npy'
 spikes_npy_uri = 'sha1://8edcf073244f2ffbd6a90f4871218c983b106eb5?label=spikes.npy'
-camera_avi_uri = ''
+camera_avi_uri = 'sha1://47a17e60eb6c04eaaafac4d2ef31a829bcf18599?label=2021_08_10_15_10_41_494305_cam_b.avi'
 mic_wav_uri = 'sha1://c59c69a73c64d49d8da795e2f6cd66d088e50935?label=mic_2021_08_10_15_10_38_554833combined.wav'
